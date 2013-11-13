d47b5fbe24dde328dd30b17332c4d479e520b46b
John Doe <adam.q.pruett@gmail.com>
2013-11-12 17:47:39 -0800

    Added a home and contact page to static rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StaticRails::Application.config.secret_key_base = '5a377a026e74217192b3b7e4cdb3bd6b7a0110d65503171b059d99e05a79cb301b30dd74a716faadb8216960f54b32c89fbf0ed408885039fc0e92e4aa92e1e9'
